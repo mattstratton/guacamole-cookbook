7fb530a81a8c2300fbc8f41249d111b75a0b5f9f
Matt Stratton <matt.stratton@gmail.com>
2015-07-21 23:24:44 -0500

    Clean up attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,1 @@
-default['guacamole']['install-source']='http://tcpdiag.dl.sourceforge.net/project/guacamole/current/source/guacamole-server-0.9.7.tar.gz'
-default['guacamole']['version']='0.9.7'
-default['tomcat']['base_version'] = 7
+default['guacamole']['version'] = '0.9.7'
