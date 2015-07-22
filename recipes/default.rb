4ae917e67a161ac5b9c1f263ec44abf44304bb5d
Matt Stratton <matt.stratton@gmail.com>
2015-07-21 22:40:37 -0500

    Add tomcat 7 stuff to recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -40,6 +40,26 @@ end
 
 # This is where we configure the guacamole-client. May need to refactor into a separate recipe later
 
+node.override['tomcat']['base_version'] = 7
+suffix = node['tomcat']['base_version'].to_i < 7 ? node['tomcat']['base_version'] : ""
+
+node.default['tomcat']['base_instance'] = "tomcat#{suffix}"
+node.default['tomcat']['user'] = 'tomcat'
+node.default['tomcat']['group'] = 'tomcat'
+node.default['tomcat']['home'] = "/usr/share/tomcat#{suffix}"
+node.default['tomcat']['base'] = "/usr/share/tomcat#{suffix}"
+node.default['tomcat']['config_dir'] = "/etc/tomcat#{suffix}"
+node.default['tomcat']['log_dir'] = "/var/log/tomcat#{suffix}"
+node.default['tomcat']['tmp_dir'] = "/var/cache/tomcat#{suffix}/temp"
+node.default['tomcat']['work_dir'] = "/var/cache/tomcat#{suffix}/work"
+node.default['tomcat']['context_dir'] = "#{node["tomcat"]["config_dir"]}/Catalina/localhost"
+node.default['tomcat']['webapp_dir'] = "/var/lib/tomcat#{suffix}/webapps"
+node.default['tomcat']['keytool'] = 'keytool'
+node.default['tomcat']['lib_dir'] = "#{node["tomcat"]["home"]}/lib"
+node.default['tomcat']['endorsed_dir'] = "#{node["tomcat"]["lib_dir"]}/endorsed"
+node.default['tomcat']['packages'] = ["tomcat#{suffix}"]
+node.default['tomcat']['deploy_manager_packages'] = ["tomcat#{suffix}-admin-webapps"]
+
 include_recipe 'tomcat::default'
 
 directory '/etc/guacamole'
