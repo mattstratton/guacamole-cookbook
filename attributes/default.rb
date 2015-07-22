deff6adb2ea4f4d2a8b562366a0b9b5688344d9f
Matt Stratton <matt.stratton@gmail.com>
2015-07-21 20:38:49 -0500

    Bump tomcat version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['guacamole']['install-source']='http://tcpdiag.dl.sourceforge.net/project/guacamole/current/source/guacamole-server-0.9.7.tar.gz'
 default['guacamole']['version']='0.9.7'
+default['tomcat']['base_version'] = 7
