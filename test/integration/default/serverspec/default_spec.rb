4e112aa20620aa8348eea61afa3eda5c0e199448
Matt Stratton <matt.stratton@gmail.com>
2015-07-21 19:21:29 -0500

    Add basic guacd tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'guacamole::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe service('guacd') do
+    it { should be_enabled }
+    it { should be_running }
   end
 end
