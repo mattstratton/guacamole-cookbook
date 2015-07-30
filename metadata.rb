b5f0c899ba5c332c58e553d4a001ee8b25216989
Matt Stratton <matt.stratton@gmail.com>
2015-07-30 19:09:54 -0400

    Bump version and add some other stuff

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mattstratton@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures guacamole'
 long_description 'Installs/Configures guacamole-server, an HTML5 remote desktop client thing'
-version '0.1.0'
+version '0.1.1'
 
 depends 'tomcat', '~> 0.17.3'
 depends 'java', '~> 1.31.0'
