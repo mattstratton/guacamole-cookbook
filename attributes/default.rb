40d66c011a186c8ef8c91af9cc25ee10e3fefea9
gmiranda23 <gmiranda@opscode.com>
2015-07-29 23:29:32 -0700

    prelim data model for per machine rendering

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,20 @@
 default['guacamole']['version'] = '0.9.7'
+
+default['guacamole']['usermap']['default'] = {
+                                              "name" => "student1",
+                                              "password" => "chef",
+                                              "machines" => {
+                                                "workstation" => {
+                                                  "hostname" => "matt-test-node.clouapp.net",
+                                                  "platform_family" => "windows",
+                                                  "username" => "chef",
+                                                  "password" => "CodeCan!"
+                                                },
+                                                "winnode" => {
+                                                  "hostname" => "matt-test-node.clouapp.net",
+                                                  "platform_family" => "windows",
+                                                  "username" => "Administrator",
+                                                  "password" => "Cod3Can!"
+                                                }
+                                              }
+                                            }
