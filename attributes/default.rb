27cff83c23219cb3adc893a0bca6432e5b11a0c5
Ian Henry <ihenry@chef.io>
2016-03-30 12:39:27 -0400

    Reconfigured to not include fake nodes as default config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,17 @@
 default['guacamole']['version'] = '0.9.9'
 
 default['guacamole']['usermap']['default'] = {
+                                               "name" => "user1",
+                                               "password" => "chef",
+                                               "machines" => {
+                                               }
+                                             }
+=begin
+
+Below is an execellent example of how to configure the node and user
+list that guacamole will use for authentication and remote access.
+
+default['guacamole']['usermap']['default'] = {
                                               "name" => "student1",
                                               "password" => "chef",
                                               "machines" => {
@@ -22,3 +33,4 @@ default['guacamole']['usermap']['default'] = {
                                                 }
                                               }
                                             }
+=end
